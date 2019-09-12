6ce07e8e540d4523b7fbcadf154a8de812cafb92
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-12 18:21:13 -0500

    [UPDATE] Adding an update to the 4-hidden_discovery.py program.

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    list = dir(hidden_4)
+    for func in list:
+        if func in list:
+            if func[0] != '_':
+                print("{:s}".format(func))
