6c4db329f11a21aef7d2b250ba7af98ae9b7163f
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-12 18:00:06 -0500

    [UPDATE] Adding an update to the 2-args.py script.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ac, av = len(sys.argv), sys.argv
+    if ac < 2:
+        print("{:d} arguments.".format(0))
+    elif ac == 2:
+        print("{:d} argument:".format(1))
+        print("{:d}: {:s}".format(1, av[1]))
+    else:
+        print("{:d}: arguments:".format(ac - 1))
+        for i in range(ac):
+            if i != 0:
+                print("{:d}: {:s}".format(i, av[i]))
