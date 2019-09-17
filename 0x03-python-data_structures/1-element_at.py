c579155bd56310c90a1e19393f415daf5be8ff97
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-17 11:12:43 -0500

    Adding the Python function 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""element_at
+Function to get an elemen of a list.
+"""
+
+
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return (None)
+    return (my_list[idx])
