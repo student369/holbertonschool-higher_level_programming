42547bd025a739a6d2f7eb86a09bb138bc83b998
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-17 11:18:36 -0500

    Adding the Python function 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""replace_in_list
+Function to replace an element in the list.
+"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
