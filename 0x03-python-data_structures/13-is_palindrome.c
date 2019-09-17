2f59586b304159d499b77332e77a7293a1f29534
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-17 16:14:00 -0500

    Adding the 13-is_palindrome.c C function.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: The linked list
+ * Author: Jose Calderon <jose.calderon@holbertonschool.com>
+ * Return: 1 or 0
+ */
+int is_palindrome(listint_t **head)
+{
+	const listint_t *current;
+	listint_t *tmp;
+	unsigned int n, i = 0, sum = 0;
+
+	current = *head;
+	tmp = *head;
+	n = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		n++;
+	}
+	int numbers[n];
+
+	while (tmp != NULL)
+	{
+		numbers[i] = tmp->n;
+		tmp = tmp->next;
+		i++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (numbers[i] == numbers[((n - 1) - i)])
+		{
+			sum -= -1;
+		}
+	}
+	if (sum == n)
+		return (1);
+	else
+		return (0);
+}
