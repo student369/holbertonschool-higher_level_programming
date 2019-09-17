9e69f0f363f18a781da57c698b397c92c8425865
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-17 12:34:48 -0500

    Adding a Python function 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""new_in_list
+
+Function to replace an element of a copy of the list.
+"""
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return (my_list.copy())
+    cp = my_list.copy()
+    cp[idx] = element
+    return (cp)
