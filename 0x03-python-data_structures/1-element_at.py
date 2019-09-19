6cf9350d3c7d19e6cd350aaea1353b3ef4d3be2a
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-19 16:52:59 -0500

    [FIX] Adding a modification to get the correct error.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,6 +5,6 @@ Function to get an elemen of a list.
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return (None)
     return (my_list[idx])
