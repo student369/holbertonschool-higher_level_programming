0cda74f8af8995be27d9003c1e2c917e474643dd
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-19 17:30:55 -0500

    [FIX]

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ Function to replace an element of a copy of the list.
 
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return (my_list.copy())
     cp = my_list.copy()
     cp[idx] = element
