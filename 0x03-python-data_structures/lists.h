c8d08e224d7449bd346df3aa467726e241d3eb27
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-19 19:12:00 -0500

    [UPDATE]

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,5 +21,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-listint_t *reverse_listint(listint_t **head);
 #endif /* LISTS_H */
