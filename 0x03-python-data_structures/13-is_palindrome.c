1670b424c367e57916241b9601e31f5be020e489
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-19 19:12:31 -0500

    [UPDATE]

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,53 +10,35 @@
 int is_palindrome(listint_t **head)
 {
 	const listint_t *current;
-	listint_t **headr;
 	listint_t *tmp;
-	unsigned int n, sum1 = 0, sum2 = 0;
+	int *numbers;
+	unsigned int n, i = 0, sum = 0;
 
 	current = *head;
-	headr = head;
+	tmp = *head;
 	n = 0;
-	sum1 = 0;
-	sum2 = 0;
 	while (current != NULL)
 	{
-		sum1 += (current->n + n);
 		current = current->next;
 		n++;
 	}
-	tmp = reverse_listint(headr);
-	n = (n - 1);
+	numbers = (int *)malloc(sizeof(int) * n);
 	while (tmp != NULL)
 	{
-		sum2 += (tmp->n + n);
+		numbers[i] = tmp->n;
 		tmp = tmp->next;
-		n--;
+		i++;
 	}
-	if (sum1 == sum2)
+	for (i = 0; i < n; i++)
+	{
+		if (numbers[i] == numbers[((n - 1) - i)])
+		{
+			sum -= -1;
+		}
+	}
+	free(numbers);
+	if (sum == n)
 		return (1);
 	else
 		return (0);
-}
-
-/**
- * reverse_listint - Function to reverse a linked list.
- * @head: The linked list
- *
- * Return: the pointer to the first element
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *tmp, *prev;
-
-	prev = NULL;
-	while (*head != NULL)
-	{
-		tmp = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = tmp;
-	}
-	*head = prev;
-	return (*head);
 }
