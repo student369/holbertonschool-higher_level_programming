980c6bf280539c97b12ff5378c477f3937ee27b0
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-20 23:35:07 -0500

    Adding a 103-python.c functions.

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,23 @@
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+/**
+ * print_python_list - check the code for Holberton School students.
+ * @p: The Python object
+ *
+ * Return: Always 0.
+ */
+void print_python_list(PyObject *p)
+{
+	return (0);
+}
+
+/**
+ * print_python_bytes - check the code for Holberton School students.
+ * @p: The Python object
+ *
+ * Return: Always 0.
+ */
+void print_python_bytes(PyObject *p)
+{
+	return (0);
+}
