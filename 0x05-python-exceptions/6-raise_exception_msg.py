1868a24ada3d488244e58ac3a31f8da8ba720254
Jose Calderon <jose.calderon@holbertonschool.com>
2019-09-30 14:49:22 -0500

    Adding the 6-raise_exception_msg.py Python function.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""raise_exception_msg
+
+Python function to raises an exception
+with a litle message.
+"""
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
