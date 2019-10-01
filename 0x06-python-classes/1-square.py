b677a3cf2bdd08e78600482d3fa5b28ef08bab43
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-01 14:22:33 -0500

    Adding the 1-square.py Python class.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module Square
+
+This module contains the Square class
+"""
+
+
+class Square:
+    """Square class
+
+    A simple empty Square class
+
+    Attributes:
+        size (str): The size if the Square
+    """
+
+    def __init__(self, size):
+        """Square constructor
+
+        Args:
+            size (str): The size of the Square
+        """
+        self.__size = size
