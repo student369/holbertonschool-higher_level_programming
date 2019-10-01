b02e288b0d82148690e94cb0e0927c05ca8c1d50
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-01 13:17:46 -0500

    Adding the 0-square.py Python class.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module Square
+
+This module contains the Square class
+"""
+
+
+class Square:
+    """Square class
+
+    A simple empty Square class
+    """
+    pass
