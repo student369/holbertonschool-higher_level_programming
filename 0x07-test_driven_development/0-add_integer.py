81b1df8c90b9006109c1f0b5a50d55b972825bea
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-03 16:23:54 -0500

    Adding the 0-add_integer.py file.

---
+++ 0x07-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module add_integer
+
+Module that contains the function add_integer
+"""
+
+
+def add_integer(a, b=98):
+    """ Returns a + b
+
+    Arguments:
+        a (int, float): The first number to sum
+        b (int, float): The second number to to sum
+    """
+    if not isinstance(a, int):
+        if not isinstance(a, float):
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if not isinstance(b, float):
+            raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return (a + b)
