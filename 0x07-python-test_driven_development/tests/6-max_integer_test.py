a805964740e8ef76158326e6235248d4fe1d07ad
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-06 14:52:43 -0500

    [UPDATE]

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,40 +6,32 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_empty(self):
+    def list_is_empty(self):
         self.assertIsInstance(max_integer([]), type(None))
 
-    def test_one_element(self):
+    def list_of_one_element(self):
         list_integers = [11]
         self.assertEqual(max_integer(l), 11)
 
-    def test_max_at_end(self):
-        list_integers = [1, 2, 3]
-        self.assertEqual(max_integer(l), 3)
-
-    def test_max_at_beginning(self):
-        list_integers = [3, 2, 1]
-        self.assertEqual(max_integer(l), 3)
-
-    def test_max_at_middle(self):
-        list_integers = [1, 3, 2]
-        self.assertEqual(max_integer(l), 3)
-
-    def test_one_negative(self):
-        list_integers = [1, 3, -1]
-        self.assertEqual(max_integer(l), 3)
-
-    def test_only_negatives(self):
+    def only_negative_numbers_in_the_list(self):
         list_integers = [-3, -2, -1]
         self.assertEqual(max_integer(l), -1)
 
-    def test_not_imports(self):
-        modu = __import__('6-max_integer')
-        self.assertFalse("^[^__]" in list(dir(modu)))
+    def one_negative_number_in_the_list(self):
+        list_integers = [1, 3, -1]
+        self.assertEqual(max_integer(l), 3)
 
-    def test_not_max(self):
-        fun = max_integer
-        self.assertFalse("^[^__]" in list(dir(fun)))
+    def max_in_the_middle(self):
+        list_integers = [1, 3, 2]
+        self.assertEqual(max_integer(l), 3)
+
+    def max_at_the_beginning(self):
+        list_integers = [3, 2, 1]
+        self.assertEqual(max_integer(l), 3)
+
+    def max_at_the_end(self):
+        list_integers = [1, 2, 3]
+        self.assertEqual(max_integer(l), 3)
 
     def test_only_integers(self):
         with self.assertRaises(TypeError) as ct:
