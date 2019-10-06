e8c48c7a45c7fa60dfa89c0d4728f5e06829cf0c
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-06 14:55:29 -0500

    [UPDATE]

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,7 +4,6 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
     def list_is_empty(self):
         self.assertIsInstance(max_integer([]), type(None))
