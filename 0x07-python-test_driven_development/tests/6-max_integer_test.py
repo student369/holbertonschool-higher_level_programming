ff23b73786fb4aef89dd732728149b1cb859bdfb
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-06 15:02:42 -0500

    [UPDATE]

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,32 +5,32 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def list_is_empty(self):
+    def test_list_is_empty(self):
         self.assertIsInstance(max_integer([]), type(None))
 
-    def list_of_one_element(self):
+    def test_list_of_one_element(self):
         list_integers = [11]
-        self.assertEqual(max_integer(l), 11)
+        self.assertEqual(max_integer(list_integers), 11)
 
-    def only_negative_numbers_in_the_list(self):
+    def test_only_negative_numbers_in_the_list(self):
         list_integers = [-3, -2, -1]
-        self.assertEqual(max_integer(l), -1)
+        self.assertEqual(max_integer(list_integers), -1)
 
-    def one_negative_number_in_the_list(self):
+    def test_one_negative_number_in_the_list(self):
         list_integers = [1, 3, -1]
-        self.assertEqual(max_integer(l), 3)
+        self.assertEqual(max_integer(list_integers), 3)
 
-    def max_in_the_middle(self):
+    def test_max_in_the_middle(self):
         list_integers = [1, 3, 2]
-        self.assertEqual(max_integer(l), 3)
+        self.assertEqual(max_integer(list_integers), 3)
 
-    def max_at_the_beginning(self):
+    def test_max_at_the_beginning(self):
         list_integers = [3, 2, 1]
-        self.assertEqual(max_integer(l), 3)
+        self.assertEqual(max_integer(list_integers), 3)
 
-    def max_at_the_end(self):
+    def test_max_at_the_end(self):
         list_integers = [1, 2, 3]
-        self.assertEqual(max_integer(l), 3)
+        self.assertEqual(max_integer(list_integers), 3)
 
     def test_only_integers(self):
         with self.assertRaises(TypeError) as ct:
