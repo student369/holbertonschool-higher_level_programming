c1a40349b20d0f72d61f8b399c8ebb6178274699
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-06 13:36:21 -0500

    [UPDATE]

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,22 +9,17 @@ def text_indentation(text):
     """Returns nothing
 
     This method prints a separated string in a new
-    lines if finish in . or ?.
+    lines if finish in . : or ?.
 
     Attributes:
         text (str): The text to split
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    find = 0
     txt_len = len(text)
     for i in range(txt_len):
         if text[i] == "." or text[i] == "?" or\
            text[i] == ":":
             print("{:s}".format(text[i]), end="\n\n")
-            find = 1
         else:
-            if text[i] == " " and find == 1:
-                find = 0
-            else:
-                print("{:s}".format(text[i]), end="")
+            print("{:s}".format(text[i]), end="")
