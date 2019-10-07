c4afe635195e339b287cd4dd94da647f7b160fa2
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-07 09:58:45 -0500

    Adding the 0-rectangle.py Python class.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module 0-rectangle
+
+Module that contains the class Rectangle
+"""
+
+
+class Rectangle:
+    """ Rectangle class
+
+    A empty Rectangle class.
+    """
+    pass
