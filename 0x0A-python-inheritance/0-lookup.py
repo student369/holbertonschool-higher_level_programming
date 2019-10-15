e8d6c366379a5c4a1a7550253422b5d74ae32d8b
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-15 11:52:50 -0500

    Adding the 0-lookup.py file.

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module 0-lookup
+
+Module that contains the function add_integer
+that adds two numbers given.
+"""
+
+
+def lookup(obj):
+    """ Returns the attributes and methods of an object
+
+    Arguments:
+        obj (obj): A Python 3 object.
+    """
+    return (dir(obj))
