47b626b4ae88d964e4d0de8718d8baef6a7421cd
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-15 17:08:15 -0500

    Adding the 5-base_geometry.py file.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module 5-base_geometry
+
+This module contains the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class
+
+    A simple empty BaseGeometry class
+    """
+    pass
