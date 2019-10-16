931db0c11a3b0ef9f14f1d8f76e7f9a355cc9929
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-16 10:50:35 -0500

    Adding the 7-save_to_json_file.py file.

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,21 @@
+"""Module 7-save_to_json_file
+that save the JSON respeentation of an
+object in a text file.
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """ Returns Nothing
+
+    A function that get the JSON representation
+    of an object and save in a file..
+
+    Args:
+        my_obj (object): The Python object.
+        filename (str): The file to store it.
+    """
+    if not isinstance(filename, str):
+        raise TypeError("It's not a valid filename.")
+    import json
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
