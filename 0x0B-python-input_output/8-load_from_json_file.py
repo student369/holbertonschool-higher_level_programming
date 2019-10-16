1808d84e85ad3b1f1df65e44fd912de73c370d05
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-16 12:14:27 -0500

    Adding the 8-load_from_json_file.py file.

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module 8-load_from_json_file
+
+Module that contains the function load_from_json_file
+that return a Python object from a JSON sting file.
+"""
+
+
+def load_from_json_file(filename):
+    """Returns an object from a JSON string.
+
+    A function that get the Python object
+    from the JSON string in an textfile.
+
+    Args:
+        filename (str): The JSON filename file..
+    """
+    if not isinstance(filename, str):
+        raise TypeError("It's not a valid filename.")
+    import json
+    with open(filename, encoding="utf-8") as f:
+        return (json.loads(f.read()))
