9c3f16d99ee1dac2c8a73944300125e79cb77490
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-19 19:51:00 -0500

    Adding the models/base.py file.

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module Base
+
+This module contains the Base class
+"""
+
+
+class Base(object):
+    """Base class
+
+    A simple empty Base class
+
+    Arguments:
+        nd_objects (int): Count the instances
+            of this class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Returns a Base object
+
+        Args:
+            id (int): An id to this base object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = self.__nb_objects
