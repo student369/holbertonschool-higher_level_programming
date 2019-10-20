a3afd2e966b49844c293c09e4457df3180ea496a
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-20 14:45:49 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,7 @@ class Rectangle(base.Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Returns the area of the Rectangle"""
+        return (self.width * self.height)
