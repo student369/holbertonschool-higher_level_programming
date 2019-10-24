e3de430e5682a1942ab5c7e5bb8df0184526fb0c
Jose Calderon <jose.calderon@holbertonschool.com>
2019-10-23 19:55:04 -0500

    [UPDATE]

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -36,6 +36,50 @@ class TestBase(unittest.TestCase):
         o = Base()
         self.assertEqual(o.id, 4)
 
+    def test_base_id5(self):
+        o = Base(89)
+        self.assertEqual(o.id, 89)
+
+    def test_json_string0(self):
+        self.assertTrue(Base.to_json_string(None))
+
+    def test_json_string1(self):
+        self.assertTrue(Base.to_json_string([]))
+
+    def test_json_string2(self):
+        self.assertTrue(
+            Base.to_json_string([{'id': 12}])
+        )
+
+    def test_json_string3(self):
+        self.assertIsInstance(
+            Base.to_json_string([{'id': 12}]),
+            str
+        )
+
+    def test_json_from_json0(self):
+        self.assertEqual(
+            Base.from_json_string(None),
+            []
+        )
+
+    def test_json_from_json1(self):
+        self.assertEqual(
+            Base.from_json_string("[]"),
+            []
+        )
+
+    def test_json_from_json2(self):
+        self.assertTrue(
+            Base.from_json_string('[{"id": 89}]')
+        )
+
+    def test_json_from_json3(self):
+        self.assertIsInstance(
+            Base.from_json_string('[{"id": 89}]'),
+            list
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
