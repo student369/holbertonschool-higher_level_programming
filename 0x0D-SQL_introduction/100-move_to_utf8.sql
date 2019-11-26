5bf7838de1f582a9a79e61bd7176cc1aee5cae04
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-25 19:38:29 -0500

    Adding the 100-move_to_utf8.sql file.

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Selection of the hbtn_0c_0 database
+USE hbtn_0c_0;
+-- Convert the table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
