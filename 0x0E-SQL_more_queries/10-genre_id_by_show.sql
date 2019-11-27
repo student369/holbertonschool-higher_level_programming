434db044d1375ce9243cfefd5a2d42245c1afcc6
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-26 20:57:31 -0500

    [FIX]

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Get the cities and display state information
-SELECT tv.title, tvg.genre_id FROM tv_shows as tv
-       RIGHT JOIN tv_show_genres as tvg ON tv.id = tvg.show_id
-       ORDER BY tv.title, tvg.genre_id;
+SELECT tv.title, tvsg.genre_id FROM tv_shows AS tv
+       RIGHT JOIN tv_show_genres AS tvsg ON tv.id = tvsg.show_id
+       ORDER BY tv.title, tvsg.genre_id ASC;
