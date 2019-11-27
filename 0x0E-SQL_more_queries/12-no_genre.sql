7fe26ac23f006479f1096bc50193ae4dc18dbed0
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-26 22:52:53 -0500

    Adding the 12-no_genre.sql file.

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- Get the titles by genre
+SELECT tv.title, tvg.genre_id FROM tv_shows AS tv
+       LEFT JOIN tv_show_genres AS tvg ON tv.id = tvg.show_id
+       WHERE tvg.show_id IS NULL
+       ORDER BY tv.title, tvg.genre_id;
