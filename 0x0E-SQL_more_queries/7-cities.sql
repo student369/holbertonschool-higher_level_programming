5d8e5f76801ea6fc413c94fe7c36f8e048e532e3
Jose Calderon <jose.calderon@holbertonschool.com>
2019-11-26 19:49:37 -0500

    Adding the 7-cities.sql file.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Create the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use de database hbtn_0d_usa
+USE hbtn_0d_usa;
+-- Create a table cities
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT AUTO_INCREMENT UNIQUE NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY(state_id) REFERENCES states(id)
+);
