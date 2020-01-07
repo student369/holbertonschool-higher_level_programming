70d99a37b034d23b69c716dd6c4bb587489e615b
Jose Calderon <jose.calderon@holbertonschool.com>
2020-01-06 21:25:13 -0500

    [FIX]

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Show the response of a get request with a header
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98;" "$1" | cat
+curl "$1" -X GET -H "X-HolbertonSchool-User-Id: 98" | cat
