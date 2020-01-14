09c8746caa96968109bd0c13928d1bbd027dee99
Jose Calderon <jose.calderon@holbertonschool.com>
2020-01-13 22:00:29 -0500

    [FIX]

--- 0x11-python-network_1/9-startwars.py
+++ 0x11-python-network_1/9-startwars.py
@@ -8,6 +8,7 @@ import sys
 if __name__ == "__main__":
     args = sys.argv[1]
     url = "https://swapi.co/api/people/?search={}".format(args)
+    r = requests.get(url)
     r = r.json()
     print("Number of results: {}".format(r.get("count")))
     for n in r.get("results"):
