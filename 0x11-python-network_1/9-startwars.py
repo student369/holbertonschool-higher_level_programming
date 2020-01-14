9c421eb9866ddebdef20fa8c34c70bd07490aa16
Jose Calderon <jose.calderon@holbertonschool.com>
2020-01-13 21:55:30 -0500

    Adding the 9-startwars.py file.

---
+++ 0x11-python-network_1/9-startwars.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module 9-startwars
+
+Python script that do an api request.
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    args = sys.argv[1]
+    url = "https://swapi.co/api/people/?search={}".format(args)
+    r = r.json()
+    print("Number of results: {}".format(r.get("count")))
+    for n in r.get("results"):
+        print(n["name"])
