97ff1cd4dbedbd9f75c40ec68868cd9c4a9d2b13
Jose Calderon <jose.calderon@holbertonschool.com>
2020-01-13 20:17:19 -0500

    [UPDATE]

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,17 @@
 Python script that handle some http errors.
 """
 import urllib.request
+import urllib.error
 import sys
 if __name__ == "__main__":
     try:
+        req = urllib.request.Request(sys.argv[1])
         url = sys.argv[1]
         with urllib.request.urlopen(url) as r:
-            if r.code >= 400:
-                print("Error code: {}".format(r.code))
-            else:
-                html = r.read()
-                print("Error code: {}".format(html.decode("UTF-8")))
+            html = r.read()
+            print(html.decode("UTF-8"))
+    except IndexError:
+        pass
+    except urllib.error.URLError as e:
+        if e.code >= 400:
+            print("Error code: {}".format(e.code))
