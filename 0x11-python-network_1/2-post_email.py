791498b30836633fde67b2e3cc260d6a40581477
Jose Calderon <jose.calderon@holbertonschool.com>
2020-01-13 19:45:08 -0500

    Adding the 2-post_email.py file.

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module 2-post_email
+
+Python script that send a POST request
+"""
+import urllib.request
+import sys
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        values = {"email": email}
+        data = urllib.parse.urlencode(values)
+        data = data.encode("ascii")
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(req) as r:
+            html = r.read()
+            print("Your email is: {}".format(html.decode("UTF-8")))
+    except IndexError:
+        pass
