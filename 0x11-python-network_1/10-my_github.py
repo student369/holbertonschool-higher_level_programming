27b6d9a7c5a25ea44c09201c5a145e5e848f6ffb
Jose Calderon <jose.calderon@holbertonschool.com>
2020-01-13 22:26:06 -0500

    [FIX]

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Module 9-startwars
+""" Module 10-my_github
 
-Python script that do an api request.
+Python script that get the id of the github account.
 """
 import requests
 import sys
 if __name__ == "__main__":
     user, psw = sys.argv[1], sys.argv[2]
-    url = "https://api.github.com/user/"
+    url = "https://api.github.com/user"
     r = requests.get(url, auth=(user, psw))
     rjson = r.json()
     print(rjson.get("id"))
