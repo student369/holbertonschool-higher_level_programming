fec7cca5e39704523c1f4f6ac1edbc8deda06c7e
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-04 09:24:13 -0500

    Adding the 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < process.argv[2]; i++) {
+  console.log('C is fun');
+}
