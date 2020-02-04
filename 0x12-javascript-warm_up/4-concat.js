048cfe0aca7af15b3f18b0d04ebdef4027d8c249
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-04 09:22:41 -0500

    Adding the 4-concat.js file.

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
+  console.log(process.argv[2] + ' is undefined');
+} else if (process.argv[3] === undefined) {
+  console.log(process.argv[2] + ' is undefined');
+} else {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
