e20717af523ace63abd48a8f082ac3e330b17edb
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-04 23:14:58 -0500

    Adding the 103-object_fct.js file.

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject['incr'] = function () {
+  let retval = this.value += 1;
+  return retval;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
