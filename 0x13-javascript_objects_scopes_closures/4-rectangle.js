32c20db3a839aa734b9159f97bee53529df01831
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-11 08:48:48 -0500

    [FIX]

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
 
-exports.Rectangle = function Rectangle (w, h) {
-  if ((w > 0 && w !== undefined) && (h > 0 && h !== undefined)) {
-    this.width = w;
-    this.height = h;
-  }
-  this.print = function (c = 'X') {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  };
-  this.double = function () {
-    this.width *= 2;
-    this.height *= 2;
-  };
-  this.rotate = function () {
-    const temp = this.width;
+  }
+  print () {
+    for (let h = this.height; h > 0; h--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let temp = this.width;
     this.width = this.height;
     this.height = temp;
-  };
+  }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 };
