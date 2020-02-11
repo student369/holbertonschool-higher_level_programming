e8fc008002fd728e86dd4a51ebe944eab91a2d25
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-11 08:52:52 -0500

    [FIX]

--- 0x14-javascript-web_scraping/8-script.js
+++ 0x14-javascript-web_scraping/8-script.js
@@ -1,6 +1,6 @@
 const $ = window.$;
 $.get('https://swapi.co/api/films/?format=json', function (data) {
-  let dataResult = data.results;
+  const dataResult = data.results;
   for (let i = 0; i < data.count; i++) {
     $('UL#list_movies').append('<li>' + dataResult[i].title + '</li>');
   }
