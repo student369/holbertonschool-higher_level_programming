e1ef23485f716ab7e88fb4aa3f5b153c53f993fa
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-11 08:49:32 -0500

    [FIX]

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-for (let i = 2; i < 4; i++) {
-  fs.readFile(process.argv[i], function read (err, data) {
-    if (err) {
-      console.log(err);
-    }
-    const content = data;
-    fs.appendFileSync('fileC', content);
-  });
-}
+let fs = require('fs');
+
+let file1 = fs.readFileSync(process.argv[2], 'utf8');
+let file2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], file1 + file2);
