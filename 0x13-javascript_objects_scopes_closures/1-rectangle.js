0b5f7e14d13510dad9cbd2867d1f9d951dee5235
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-11 08:45:10 -0500

    [FIX]

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-exports.Rectangle = function Rectangle (w, h) {
-  this.width = w;
-  this.height = h;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
