a6cc84ac42fba6523e377aa5593326accb9ab124
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-12 06:09:48 -0500

    [FIX]

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,9 +5,9 @@ const request = require('request');
 request(process.argv[2], function (err, res, body) {
   if (err) console.log(err);
   else {
-    let completetasks = {};
-    let todos = JSON.parse(body);
-    for (let task in todos) {
+    const completetasks = {};
+    const todos = JSON.parse(body);
+    for (const task in todos) {
       if (todos[task].completed === true) {
         if (completetasks[todos[task].userId]) completetasks[todos[task].userId]++;
         else completetasks[todos[task].userId] = 1;
