c5a90ed1f7bf7697cf2139a1bd0e9ddda47b3ccc
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-12 06:10:03 -0500

    [FIX]

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,8 +5,8 @@ const request = require('request');
 request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
   if (err) console.log(err);
   else {
-    let characters = JSON.parse(body).characters;
-    for (let character in characters) {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
       request(characters[character], function (err, res, body) {
         if (err) console.log(err);
         else {
