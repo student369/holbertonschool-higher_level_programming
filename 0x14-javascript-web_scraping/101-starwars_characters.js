386e18609a0e311d4251990ba6b023c986d681ce
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-12 06:07:14 -0500

    [FIX]

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,27 @@
-const $ = window.$;
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-  const dataResult = data.results;
-  for (let i = 0; i < data.count; i++) {
-    $('UL#list_movies').append('<li>' + dataResult[i].title + '</li>');
+#!/usr/bin/node
+
+const request = require('request');
+
+request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) console.log(err);
+  else {
+    let characters = JSON.parse(body).characters;
+    let promiselist = [];
+    for (let character in characters) {
+      const namepromise = new Promise(resolve => {
+        request(characters[character], function (err, res, body) {
+          if (err) console.log(err);
+          else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      promiselist.push(namepromise);
+    }
+    Promise.all(promiselist).then(function (namelist) {
+      for (let name in namelist) {
+        console.log(namelist[name]);
+      }
+    });
   }
 });
