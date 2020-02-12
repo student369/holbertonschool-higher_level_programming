b05392c125f7bdc10298b18223693982032b20f3
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-12 06:10:19 -0500

    [FIX]

--- 0x14-javascript-web_scraping/102-search_twitter.js
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -5,22 +5,22 @@ const request = require('request');
 const key = Buffer.from(process.argv[2] + ':' + process.argv[3]).toString('base64');
 
 function callsearch (oauthkey) {
-  let searchoptions = {
-    'url': 'https://api.twitter.com/1.1/search/tweets.json',
-    'headers': {
-      'Authorization': 'Bearer ' + oauthkey,
+  const searchoptions = {
+    url: 'https://api.twitter.com/1.1/search/tweets.json',
+    headers: {
+      Authorization: 'Bearer ' + oauthkey,
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
     },
-    'qs': {
-      'q': process.argv[4],
-      'count': 5
+    qs: {
+      q: process.argv[4],
+      count: 5
     }
   };
 
   request.get(searchoptions, function (err, res, body) {
     if (err) console.log('error', err);
     else {
-      let results = JSON.parse(body);
+      const results = JSON.parse(body);
       for (let result in results.statuses) {
         result = results.statuses[result];
         console.log(`[${result.id}] ${result.text} by ${result.user.name}`);
@@ -29,13 +29,13 @@ function callsearch (oauthkey) {
   });
 }
 
-let options = {
-  'url': 'https://api.twitter.com/oauth2/token',
-  'headers': {
-    'Authorization': 'Basic ' + key,
+const options = {
+  url: 'https://api.twitter.com/oauth2/token',
+  headers: {
+    Authorization: 'Basic ' + key,
     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
   },
-  'body': 'grant_type=client_credentials'
+  body: 'grant_type=client_credentials'
 };
 
 request.post(options, function (err, res, body) {
