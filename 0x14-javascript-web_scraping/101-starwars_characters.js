db6d4369ad9229e2b70001768c1c67c0f300d8bc
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-12 06:10:09 -0500

    [FIX]

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,9 +5,9 @@ const request = require('request');
 request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
   if (err) console.log(err);
   else {
-    let characters = JSON.parse(body).characters;
-    let promiselist = [];
-    for (let character in characters) {
+    const characters = JSON.parse(body).characters;
+    const promiselist = [];
+    for (const character in characters) {
       const namepromise = new Promise(resolve => {
         request(characters[character], function (err, res, body) {
           if (err) console.log(err);
@@ -19,7 +19,7 @@ request('http://swapi.co/api/films/' + process.argv[2], function (err, res, body
       promiselist.push(namepromise);
     }
     Promise.all(promiselist).then(function (namelist) {
-      for (let name in namelist) {
+      for (const name in namelist) {
         console.log(namelist[name]);
       }
     });
