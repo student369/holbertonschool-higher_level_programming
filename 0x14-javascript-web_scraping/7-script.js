5c908295d4ea9c48cf3ad6eb9144fad561b2cf26
Jose Calderon <jose.calderon@holbertonschool.com>
2020-02-12 00:14:20 -0500

    [FIX]

--- 0x14-javascript-web_scraping/7-script.js
+++
@@ -1,7 +0,0 @@
-const $ = window.$;
-
-$(function () {
-  $.get('https://swapi.co/api/people/5/?format=json', function (data, textStatus) {
-    $('DIV#character').text(data.name);
-  });
-});
--- 0x14-javascript-web_scraping/8-script.js
+++
@@ -1,7 +0,0 @@
-const $ = window.$;
-$.get('https://swapi.co/api/films/?format=json', function (data) {
-  const dataResult = data.results;
-  for (let i = 0; i < data.count; i++) {
-    $('UL#list_movies').append('<li>' + dataResult[i].title + '</li>');
-  }
-});
